39d4538d962b1fa447368d9038f6a010d3902142
levicrouch <levi.crouch@gmail.com>
2020-01-11 17:05:01 -0800

    Adding Roboto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import mainbg from './images/IMG_0617.JPG';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={mainbg} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
